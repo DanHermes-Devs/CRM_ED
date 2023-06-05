cd59a98d1175dfd26dc32d1da8925e20e9b5ccd9
Alberto <80308086+DanHermes-Devs@users.noreply.github.com>
2023-06-05 12:08:40 -0600

    Actualizar tVenta

--- app/Http/Controllers/API/ventas/VentasController.php
+++ app/Http/Controllers/API/ventas/VentasController.php
@@ -281,7 +281,7 @@ class VentasController extends Controller
         $venta->save();
 
         // Si tVenta es NUEVA VENTA y tVenta es Renovacion, me crea los recibos de pago, de lo contrario, no hace nada y si $request->FrePago es diferente de null, me crea los recibos de pago
-        if($venta->tVenta === 'VENTA NUEVA' || $venta->tVenta === 'RENOVACION' || $venta->FrePago !== null){
+        if($venta->tVenta === 'VENTA' || $venta->tVenta === 'RENOVACION' || $venta->FrePago !== null){
             $frecuenciaPago = $request->input('FrePago');
             $this->crearRecibosPago($venta, $frecuenciaPago);
         }
