e2b9f481f0f9c05df9df810d8bec0cd772789a1a
luisalv182 <110621901+luisalv182@users.noreply.github.com>
2023-06-08 12:08:18 -0600

    Corrección
    Corrección

--- resources/views/crm/modulos/educacion/uin/actions.blade.php
+++ resources/views/crm/modulos/educacion/uin/actions.blade.php
@@ -1,11 +1,11 @@
 <div class="hstack gap-3 fs-20">
     @can('ver-educacion')
-        <a href="{{ route('educacion-uin.show', $id) }}" class="link-success">
+        <a href="{{ route('educacion-uin.show', $coti->contact_id) }}" class="link-success">
             <i class="ri-eye-line"></i>
         </a>
     @endcan
     @can('editar-educacion')
-        <a href="{{ route('educacion-uin.edit', $id) }}" class="link-secondary">
+        <a href="{{ route('educacion-uin.edit', $coti->contact_id) }}" class="link-secondary">
             <i class="ri-pencil-line"></i>
         </a>
     @endcan
