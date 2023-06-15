d8be5b90f5fa7bdfe3ead709f23bb17f4970218b
Dan Hermes <danhermes2019@outlook.com>
2023-06-15 17:33:29 -0600

    Correccion en la importacion de ventas

--- app/Imports/VentasImport.php
+++ app/Imports/VentasImport.php
@@ -22,127 +22,97 @@ class VentasImport implements ToModel, WithValidation
     */
     public function model(array $row)
     {
-        // Verifica si el ID ya existe en la base de datos
-        $ventaExistente = Venta::find($row[0]);
-        if ($ventaExistente) {
-            // Si el ID ya existe, asigna de forma automatica el ID al siguiente disponible
-            $row[0] = Venta::max('id') + 1;
-        }
-
         $venta = new Venta([
-            'id' => $row[0],
-            'contactId' => $row[1],
-            'UGestion' => $row[2],
-            'Fpreventa' => $this->parseDateTime($row[3], 'd/m/Y H:i'),
-            'campana' => $row[4],
-            'LoginOcm' => $row[5],
-            'LoginIntranet' => $row[6],
-            'NombreAgente' => $row[7],
-            'Supervisor' => $row[8],
-            'Codificacion' => $row[9],
-            'Nombre' => $row[10],
-            'ApePaterno' => $row[11],
-            'ApeMaterno' => $row[12],
-            'fNacimiento' => $this->parseDate($row[13], 'd/m/Y'),
-            'Edad' => $row[14],
-            'Genero' => $row[15],
-            'RFC' => $row[16],
-            'Homoclave' => $row[17],
-            'CURP' => $row[18],
-            'TelFijo' => $row[19],
-            'TelCelular' => $row[20],
-            'TelEmergencias' => $row[21],
-            'Correo' => $row[22],
-            'Calle' =>  $row[23],
-            'NumExt' => $row[24],
-            'NumInt' => $row[25],
-            'Colonia' => $row[26],
-            'AlMun' => $row[27],
-            'Estado' => $row[28],
-            'CP' => $row[29],
-            'Marca' => $row[30],
-            'SubMarca' => $row[31],
-            'Modelo' => $row[32],
-            'nSerie' => $row[33],
-            'nMotor' => $row[34],
-            'nPlacas' => $row[35],
-            'Segmento' => $row[36],
-            'Legalizado' => $row[37],
-            'nCotizacion' => $row[38],
-            'FinVigencia' => !empty($row[39]) ? Carbon::createFromFormat('d/m/Y', $row[39])->format('Y-m-d') : null,
-            'FfVigencia' => !empty($row[40]) ? Carbon::createFromFormat('d/m/Y', $row[40])->format('Y-m-d') : null,
-            'tPoliza' => $this->parseDateTime($row[41], 'd/m/Y H:i'),
-            'Paquete' => $row[42],
-            'nPoliza' => $row[43],
-            'Aseguradora' => $row[44],
-            'fPago' => $row[45],
-            'FrePago' => $row[46],
-            'tTarjeta' => $row[47],
-            'nTarjeta' => $row[48],
-            'fvencimiento' => $row[49],
-            'PncTotal' => $row[50],
-            'NombreDeCliente' => $row[51],
-            'tVenta' => $row[52],
-            'MesBdd' => $row[53],
-            'AnioBdd' => $row[54],
-            'noPago' => $row[55],
-            'FechaProximoPago' => $this->parseDate($row[56] ?? null, 'd/m/Y'),
-            'FechaPagoReal' => $this->parseDate($row[57] ?? null, 'd/m/Y'),
-            'PrimaNetaCobrada' => $row[58],
-            'AgenteCob' => $row[59],
-            'TipoPago' => $row[60],
-            'EstadoDePago' => $row[61],
+            'contactId' => $row[0],
+            'Nombre' => $row[1],
+            'ApePaterno' => $row[2],
+            'ApeMaterno' => $row[3],
+            'fNacimiento' => $this->parseDate($row[4], 'd/m/Y'),
+            'Genero' => $row[5],
+            'RFC' => $row[6],
+            'TelFijo' => $row[7],
+            'TelCelular' => $row[8],
+            'Correo' => $row[9],
+            'Calle' =>  $row[10],
+            'NumExt' => $row[11],
+            'AlMun' => $row[12],
+            'Estado' => $row[13],
+            'CP' => $row[14],
+            'Marca' => $row[15],
+            'Modelo' => $row[16],
+            'nSerie' => $row[17],
+            'FinVigencia' => !empty($row[18]) ? Carbon::createFromFormat('d/m/Y', $row[18])->startOfDay()->toDateString() : null,
+            'FfVigencia' => !empty($row[18]) ? Carbon::createFromFormat('d/m/Y', $row[18])->startOfDay()->addYear()->toDateString() : null,
+            'Paquete' => $row[19],
+            'nPoliza' => $this->parseDateTime($row[20], 'd/m/Y H:i'),
+            'Aseguradora' => $row[21],
+            'FrePago' => $row[22],
+            'tVenta' => $row[23],
+            'MesBdd' => $row[24],
+            'AnioBdd' => $row[25],
             'created_at' => Carbon::now()
         ]);
 
         $venta->save();
 
         // Verificamos si ya existen recibos de pago para la venta
-        $recibos = Receipt::where('venta_id', $venta->id)->count();
+        // $recibos = Receipt::where('venta_id', $venta->id)->count();
 
-        // Si no hay recibos existentes, crea nuevos recibos
-        if ($recibos === 0) {
-            // Creamos un arreglo con las frecuencias de pago
-            $frecuenciaPagos = [
-                'ANUAL' => 1,
-                'SEMESTRAL' => 2,
-                'TRIMESTRAL' => 4,
-                'CUATRIMESTRAL' => 3,
-                'MENSUAL' => 12
-            ];
+        // // Si no hay recibos existentes, crea nuevos recibos
+        // if ($recibos === 0) {
+        //     // Creamos un arreglo con las frecuencias de pago
+        //     $frecuenciaPagos = [
+        //         'ANUAL' => 1,
+        //         'SEMESTRAL' => 2,
+        //         'TRIMESTRAL' => 4,
+        //         'CUATRIMESTRAL' => 3,
+        //         'MENSUAL' => 12
+        //     ];
 
-            // Convertimos frecuenciaPago en Mayusculas
-            $frecuenciaPago = strtoupper($venta->FrePago);
+        //     // Convertimos frecuenciaPago en Mayusculas
+        //     $frecuenciaPago = strtoupper($venta->FrePago);
 
-            // Verificar si la frecuencia de pago es válida
-            if (!array_key_exists($frecuenciaPago, $frecuenciaPagos)) {
-                $frecuenciaPago = null;
-            }
+        //     // Verificar si la frecuencia de pago es válida
+        //     if (!array_key_exists($frecuenciaPago, $frecuenciaPagos)) {
+        //         $frecuenciaPago = null;
+        //     }
 
-            if ($frecuenciaPago !== null) {
-            
-                $numRecibos = $frecuenciaPagos[$frecuenciaPago];
+        //     if ($frecuenciaPago !== null) {
+        //         $numRecibos = $frecuenciaPagos[$frecuenciaPago];
+        //         $finVigencia = Carbon::parse($venta->FinVigencia);
 
-                for ($i = 1; $i <= $numRecibos; $i++) {
-                    $finVigencia = Carbon::parse($venta->FinVigencia);
-                    $fechaProximoPago = $finVigencia->addMonths($i);
+        //         $primerReciboAnualAsignado = false;
 
-                    $receipt = new Receipt([
-                        'venta_id' => $venta->id,
-                        'num_pago' => $i,
-                        'fre_pago' => $venta->FrePago,
-                        'fecha_proximo_pago' => $i > 1 ? $fechaProximoPago : null,
-                        'fecha_pago_real' => $venta->Fpreventa,
-                        'prima_neta_cobrada' => $venta->PrimaNetaCobrada,
-                        'agente_cob_id' => null,
-                        'tipo_pago' => $i == $numRecibos ? 'LIQUIDADO' : 'PAGO PARCIAL',
-                        'estado_pago' => 'PENDIENTE'
-                    ]);
+        //         for ($i = 1; $i <= $numRecibos; $i++) {
+        //             $mesesPorRecibo = 12 / $numRecibos; // Cantidad de meses por recibo
+        //             $fechaProximoPago = $finVigencia->copy()->addMonthsNoOverflow($mesesPorRecibo * ($i - 1));
 
-                    $receipt->save();
-                }
-            }
-        }
+        //             $fechaProximoPago = $i == 1 ? $finVigencia : $fechaProximoPago;
+
+        //             // Hago que el primer recibo se le asigne el agente que hizo la venta
+
+        //             $receipt = new Receipt([
+        //                 'venta_id' => $venta->id,
+        //                 'num_pago' => $i,
+        //                 'fre_pago' => $venta->FrePago,
+        //                 'fecha_proximo_pago' => $i > 1 ? $fechaProximoPago : $finVigencia,
+        //                 'fecha_pago_real' => $venta->Fpreventa,
+        //                 'prima_neta_cobrada' => $venta->PncTotal,
+        //                 'agente_cob_id' => $i == 1 ? $venta->agent->id ?? null : null,
+        //                 'tipo_pago' => $i == $numRecibos ? 'LIQUIDADO' : 'PAGO PARCIAL',
+        //                 'estado_pago' => $i == 1 && $frecuenciaPago != 'ANUAL' && !$primerReciboAnualAsignado ? 'PAGADO' : 'PENDIENTE',
+        //                 'contactId' => $venta->contactId,
+        //             ]);
+
+        //             // Después de asignar el estado de pago, para marcar el primer recibo anual como asignado
+        //             if ($i == 1 && $frecuenciaPago == 'ANUAL') {
+        //                 $primerReciboAnualAsignado = true;
+        //             }
+
+        //             $receipt->save();
+        //         }
+        //     }
+        // }
 
         // Devuelve la instancia de Venta
         return $venta;
