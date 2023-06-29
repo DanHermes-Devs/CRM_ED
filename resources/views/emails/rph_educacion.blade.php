6f8b956784bcdc9b10bf35d46c2188d47fb1dd5e
DanielGlozgar <131414742+DanielGlozgar@users.noreply.github.com>
2023-06-28 19:11:51 -0600

    RPH UIN
    Se corrige error html, se detecta que son los espacios que impiden el armado del html.

--- resources/views/emails/rph_educacion.blade.php
+++ resources/views/emails/rph_educacion.blade.php
@@ -1,6 +1,7 @@
 @component('mail::message')
 # RPH | UNIVERSIDAD INSURGENTES
 
+
 <table style="width: 900px; margin-bottom: 2rem; border-collapse: collapse;">
     <thead>
         <tr style="border-bottom: 1px solid #dee2e6;">
@@ -15,25 +16,54 @@
     </thead>
     <tbody>
         @if (!empty($processedData) && is_array($processedData))
+            @php
+                $totalLeads = 0;
+                $totalLamadas = 0;
+                $totalPreventas = 0 ;
+                $totalVentas = 0;
+            @endphp
             @for ($i = 0; $i < count($processedData['keys']); $i++)
                 <tr style="border-bottom: 1px solid #dee2e6;">
                     <td style="padding: .75rem;">{{$processedData['keys'][$i]}}</td>
                     <td style="padding: .75rem;">{{$processedData['leads'][$processedData['keys'][$i]]}}</td>
                     <td style="padding: .75rem;">{{$processedData['llamadas'][$processedData['keys'][$i]]}}</td>
-                    @if $processedData['leads'][$processedData['keys'][$i]] > 0
+                    @if (isset($processedData['leads'][$processedData['keys'][$i]]) &&  $processedData['leads'][$processedData['keys'][$i]] > 0)
                         <td style="padding: .75rem;">{{number_format(($processedData['llamadas'][$processedData['keys'][$i]]/$processedData['leads'][$processedData['keys'][$i]])*100,0).'%'}}</td>
                     @else
                         <td style="padding: .75rem;">{{'0%'}}</td>
                     @endif
                     <td style="padding: .75rem;">{{$processedData['preventas'][$processedData['keys'][$i]]}}</td>
-                    @if $processedData['leads'][$processedData['keys'][$i]] > 0
+                    @if (isset($processedData['leads'][$processedData['keys'][$i]]) &&  $processedData['leads'][$processedData['keys'][$i]] > 0)
                         <td style="padding: .75rem;">{{number_format(($processedData['preventas'][$processedData['keys'][$i]]/$processedData['leads'][$processedData['keys'][$i]])*100,1).'%'}}</td>
                     @else
-                        <td style="padding: .75rem;">{{'0%'}}</td
+                        <td style="padding: .75rem;">{{'0%'}}</td>
                     @endif
                     <td style="padding: .75rem;">{{$processedData['ventas'][$processedData['keys'][$i]]}}</td>
                 </tr>
+                @php
+                    $totalLamadas = $totalLamadas + $processedData['llamadas'][$processedData['keys'][$i]];
+                    $totalLeads = $totalLeads + $processedData['leads'][$processedData['keys'][$i]];
+                    $totalVentas = $totalVentas + $processedData['ventas'][$processedData['keys'][$i]];
+                    $totalPreventas = $totalPreventas + $processedData['preventas'][$processedData['keys'][$i]];
+                @endphp
             @endfor
+            <tr style="border-bottom: 1px solid #dee2e6;">
+                <td style="padding: .75rem;">{{'Total'}}</td>
+                <td style="padding: .75rem;">{{$totalLeads}}</td>
+                <td style="padding: .75rem;">{{$totalLamadas}}</td>
+                @if ($totalLeads > 0)
+                    <td style="padding: .75rem;">{{number_format(($totalLamadas/$totalLeads)*100,0).'%'}}</td>
+                @else
+                    <td style="padding: .75rem;">{{'0%'}}</td>
+                @endif
+                <td style="padding: .75rem;">{{$totalPreventas}}</td>
+                @if ($totalLeads > 0)
+                    <td style="padding: .75rem;">{{number_format(($totalPreventas/$totalLeads)*100,0).'%'}}</td>
+                @else
+                    <td style="padding: .75rem;">{{'0%'}}</td>
+                @endif
+                <td style="padding: .75rem;">{{$totalVentas}}</td>
+            </tr>
         @endif
     </tbody>
 </table>
